1c358cc2dd3700fb52c7a11babae771b764d9f7c
Jeff Ripke <jripke74@gmail.com>
2022-12-01 11:38:38 -0800

    add fetch data logic

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { sendCardData } from './store/cart-slice';
+import { sendCardData, fetchCartData } from './store/cart-actions';
 
 let isInitial = true;
 
@@ -14,6 +14,10 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
     if (isInitial) {
@@ -30,7 +34,7 @@ function App() {
         <Notification
           status={notification.status}
           title={notification.title}
-          massage={notification.message}
+          message={notification.message}
         />
       )}
       <Layout>
